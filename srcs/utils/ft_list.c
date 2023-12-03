c671fc707b60f850447b942ca1fb2b76c9f4a0f6
StanX19 <tanshanchien@gmail.com>
2023-12-03 19:08:45 +0800

    linked list

---
+++ srcs/utils/ft_list.c
@@ -0,0 +1,64 @@
+#include "so_long.h"
+
+static t_node	*new_node(void *ptr)
+{
+    t_node	*new;
+	
+	new = (t_node *)malloc(sizeof(t_node));
+    if (new == 0)
+	{
+        ft_printf("ERROR: New node: failed_to_malloc\n");
+        return 0;
+    }
+    new->ptr = ptr;
+    new->next = 0;
+    return new;
+}
+
+void	ft_list_add(t_linked_list *list, void *ptr)
+{
+    t_node *new = new_node(ptr);
+    if (new == 0)
+        return;
+    new->next = list->head;
+    list->head = new;
+}
+
+void	ft_list_remove(t_linked_list *list, void *ptr)
+{
+    t_node *current;
+    t_node *prev;
+	
+	current = list->head;
+	prev = 0;
+    while (current != 0) {
+        if (current->ptr == ptr) {
+            if (prev == 0) {
+                list->head = current->next;
+            } else {
+                prev->next = current->next;
+            }
+
+            free(current);
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
+}
+
+void	ft_free_list(t_linked_list *list, void(*free_func)(void*))
+{
+    t_node *current;
+    t_node *next;
+
+	current = list->head;
+    while (current != 0) {
+        next = current->next;
+		free_func(current->ptr);
+        free(current);
+        current = next;
+    }
+
+    list->head = 0;
+}

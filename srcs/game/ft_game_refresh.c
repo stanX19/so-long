bbf2eb5746d5c237d56f745c47b9157fa9c0c0fb
StanX19 <tanshanchien@gmail.com>
2023-12-15 10:32:26 +0800

    smoothing settle

--- srcs/game/ft_game_refresh.c
+++ srcs/game/ft_game_refresh.c
@@ -11,17 +11,17 @@ static t_vec2	get_offset(t_map *map, t_image *bkg, t_itbl *subject)
 	new_ofst.y = bkg->height / 2 - new_ofst.y;
 	diff.x = new_ofst.x - ofst.x;
 	diff.y = new_ofst.y - ofst.y;
-	diff.x = sign(diff.x) * max(abs(diff.x) / 100, abs(diff.x) - bkg->width / 2);
-	diff.y = sign(diff.y) * max(abs(diff.y) / 100, abs(diff.y) - bkg->height / 2);
+	diff.x = sign(diff.x) * max(abs(diff.x) / 100, abs(diff.x) - bkg->width / 2 + 100);
+	diff.y = sign(diff.y) * max(abs(diff.y) / 100, abs(diff.y) - bkg->height / 2 + 100);
 	ofst.x += diff.x;
 	ofst.y += diff.y;
 	ofst.x = min(0, ofst.x);
 	ofst.y = min(0, ofst.y);
 	ofst.x = max(bkg->width - map->bkg_img->width, ofst.x);
 	ofst.y = max(bkg->height - map->bkg_img->height, ofst.y);
-	// ft_printf("ofst: (%i, %i), new_ofst: (%i, %i), max: (%i, %i)\n",
+	// ft_printf("ofst: (%i, %i), new_ofst: (%i, %i), diff: (%i, %i)\n",
 	// 	ofst.x, ofst.y, new_ofst.x, new_ofst.y,
-	// 	bkg->width - map->bkg_img->width, bkg->height - map->bkg_img->height
+	// 	diff.x, diff.y
 	// );
 	return ofst;
 }

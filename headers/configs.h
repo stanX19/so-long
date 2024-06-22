07caaefcccc9a88e7292d82cdf19404d82316add
Tan Shan Chien <tanshanchien@gmail.com>
2024-06-22 12:20:36 +0800

    speed change

--- headers/configs.h
+++ headers/configs.h
@@ -6,7 +6,7 @@
 /*   By: shatan <shatan@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 17:52:13 by shatan            #+#    #+#             */
-/*   Updated: 2024/06/20 12:04:59 by shatan           ###   ########.fr       */
+/*   Updated: 2024/06/22 12:20:03 by shatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 typedef enum s_speed
 {
 	SPEED_DEFAULT = 2,
-	SPEED_PLAYER = 2,
+	SPEED_PLAYER = 4,
 	SPEED_SLIME = 1,
 	SPEED_BEE = 4,
 	SPEED_WOLF = 4,
